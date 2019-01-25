8e9be834407602bf59c68a16a741c81fb208cbf9
LMongoose <33644529+LMongoose@users.noreply.github.com>
2019-01-25 13:28:55 -0200

    Add files via upload

--- hibpAPI.py
+++ hibpAPI.py
@@ -1,10 +1,15 @@
-﻿# Original author: Lucas Soares Pellizzaro on 2019-01-17
+﻿# Original author: Lucas Soares Pellizzaro on 2019-01-24
 
-import re
+import time, re
 
+# Constants
 _CONNECTION = None
+_USERAGENT = ""
+_INTERVAL = 3
 _PROXYURL = ""
+_BASE_APIURL = "https://haveibeenpwned.com/api/v2/"
 
+# Exceptions
 class ConnectionError(Exception):
 	"""
 	Exception raised by connection not started properly.
@@ -12,15 +17,16 @@ class ConnectionError(Exception):
 		message -- explanation of the error
 	"""
 	def __init__(self):
-		self.msg1 = "A connection has not been set,"
-		self.msg2 = " use 'startConnection()' to start a connection"
-		self.msg3 = " and 'setProxy(proxy_url)' if you are using a proxy."
-		self.message = self.msg1+self.msg2+self.msg3
+		_msg1 = "A connection has not been set,"
+		_msg2 = " use 'startConnection()' to start a connection"
+		_msg3 = " and 'setProxy(proxy_url)' if you are using a proxy."
+		self.message = _msg1+_msg2+_.msg3
 
-def _getData(p_url):
+# Local functions
+def _getJsonResponse(p_url):
+	time.sleep(_INTERVAL)
 	# Starts the request
-	baseurl = "https://haveibeenpwned.com/api/v2/"
-	this_request = _CONNECTION.request("GET", url=baseurl+p_url)
+	this_request = _CONNECTION.request("GET", url=p_url)
 	# Checks the response status and returns output
 	if(str(this_request.status) == "200"):
 		import json
@@ -41,28 +47,57 @@ def _getData(p_url):
 		}
 	return func_output
 
+def _getTextResponse(p_url):
+	time.sleep(_INTERVAL)
+	# Starts the request
+	this_request = _CONNECTION.request("GET", url=p_url)
+	# Checks the response status and returns output
+	if(str(this_request.status) == "200"):
+		func_output = {
+			"statuscode": 200,
+			"output": this_request.data.decode("utf-8")
+		}
+	elif(str(this_request.status) == "404"):
+		func_output = {
+			"statuscode": 404,
+			"output": ""
+		}
+	else:
+		func_output = {
+			"statuscode": this_request.status,
+			"output": None
+		}
+	return func_output
+
 # Public functions
 def startConnection():
 	import urllib3
 	urllib3.disable_warnings()
 	from urllib3 import make_headers
-	USER_AGENT = "hibpAPI/1.0 (A Python interface to the public hibp API)"
-	headers = make_headers(keep_alive=False, user_agent=USER_AGENT)
-	global _CONNECTION
-	if(_PROXYURL != ""):
-		from urllib3 import ProxyManager
-		_CONNECTION = urllib3.ProxyManager(proxy_url=_PROXYURL, headers=headers)
+	if(_USERAGENT==""):
+		print("The user agent has not been set.")
 	else:
-		_CONNECTION = urllib3.PoolManager(headers=headers)
+		headers = make_headers(keep_alive=False, user_agent=_USERAGENT)
+		global _CONNECTION
+		if(_PROXYURL != ""):
+			from urllib3 import ProxyManager
+			_CONNECTION = urllib3.ProxyManager(proxy_url=_PROXYURL, headers=headers)
+		else:
+			_CONNECTION = urllib3.PoolManager(headers=headers)
 
 def setProxy(p_proxyurl):
-	pattern = """(?:http|https)(?:\:\/\/)(?:[a-z]*(?:\.)?){4}(?:\:[0-9]{1,5})"""
+	pattern = """(?:http|https)(?:\:\/\/)(?:[a-z]*(?:\.)?){5}(?:\:[0-9]{1,5})"""
 	if(re.match(pattern, p_proxyurl)):
 		global _PROXYURL
 		_PROXYURL = p_proxyurl
 	else:
 		print("The provided proxy url is invalid.")
 
+def setUserAgent(p_useragent):
+	global _USERAGENT
+	_USERAGENT = p_useragent
+
+# Request functions
 def getAllBreachesForAccount(p_account):
 	"""
 	Returns a list of all breaches that 'p_account' has been involved in.
@@ -71,8 +106,8 @@ def getAllBreachesForAccount(p_account):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "breachedAccount/"+p_account+"?includeUnverified=true"
-			return _getData(url)
+			url = _BASE_APIURL+"breachedAccount/"+p_account+"?includeUnverified=true"
+			return _getJsonResponse(url)
 	except ConnectionError as e:
 		print(e.message)
 
@@ -85,8 +120,8 @@ def getAllBreachesForAccountOfDomain(p_account, p_domain):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "breachedAccount/"+p_account+"?domain="+p_domain+"?includeUnverified=true"
-			return _getData(url)
+			url = _BASE_APIURL+"breachedAccount/"+p_account+"?domain="+p_domain+"?includeUnverified=true"
+			return _getJsonResponse(url)
 	except ConnectionError as e:
 		print(e.message)
 
@@ -99,8 +134,8 @@ def getAllPastesForAccount(p_account):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "pasteaccount/"+p_account
-			return _getData(url)
+			url = _BASE_APIURL+"pasteaccount/"+p_account
+			return _getJsonResponse(url)
 	except ConnectionError as e:
 		print(e.message)
 
@@ -112,8 +147,8 @@ def getAllBreaches():
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "breaches/"
-			return _getData(url)
+			url = _BASE_APIURL+"breaches/"
+			return _getJsonResponse(url)
 	except ConnectionError as e:
 		print(e.message)
 
@@ -125,8 +160,8 @@ def getAllBreachesOfDomain(p_domain):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "breaches/"+"?domain="+p_domain
-			return _getData(url)
+			url = _BASE_APIURL+"breaches/"+"?domain="+p_domain
+			return _getJsonResponse(url)
 	except ConnectionError as e:
 		print(e.message)
 
@@ -138,8 +173,8 @@ def getSingleBreachedSite(p_name):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "breach/"+p_name
-			return _getData(url)
+			url = _BASE_APIURL+"breach/"+p_name
+			return _getJsonResponse(url)
 	except ConnectionError as e:
 		print(e.message)
 
@@ -153,7 +188,22 @@ def getAllDataClasses():
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			url = "dataclasses/"
-			return _getData(url)
+			url = _BASE_APIURL+"dataclasses/"
+			return _getJsonResponse(url)
+	except ConnectionError as e:
+		print(e.message)
+
+def getPwnedPasswords(p_hash5):
+	"""
+	Returns a list(string) with the suffix of every hash beginning with the
+	prefix 'p_hash5', followed by a count of how many times it appears in 
+	the data set separated from the suffix with ";".
+	"""
+	try:
+		if(_CONNECTION == None):
+			raise ConnectionError
+		else:
+			url = "https://api.pwnedpasswords.com/range/"+p_hash5
+			return _getTextResponse(url)
 	except ConnectionError as e:
 		print(e.message)
